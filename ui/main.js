7ec29418a3102677e306fa73eab31f11487cf840
vijaysai005 <vijay.sai005@gmail.com>
2017-01-14 03:20:15 +0530

    changes made respectively

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+
+// Counter Code
+
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function(){
+
+	// Create a counter request
+
+	// Capture the response and store it in a variable
+
+	// render the variable in the correct span
+	counter = counter + 1;
+	var span = document.getElementById('count');
+	span.innerHTML = counter.toString()
+}
